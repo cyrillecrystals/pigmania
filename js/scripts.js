458ed35e0b5479c0553fb773de642a5ac367741a
Kendall Cyrille <kendallnjiraini@icloud.com>
2018-03-02 15:46:59 +0300

    Add folder structure

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//business logic
+var player1 = "";
+var player2 = "";
+
+var throwdice = function() {
+  return Math.floor(6 * Math.random()) + 1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+
+Player.prototype.rollone = function() {
+    if (this.roll === 1) {
+      this.tempscore = 0;
+      alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
